ee290209eadf024e287a1d73de1616907b5c9cdf
lissk0 <105405909+LiSSk0@users.noreply.github.com>
2026-06-27 17:13:10 +0300

    added logging

--- config/config_settings.py
+++ config/config_settings.py
@@ -1,5 +1,6 @@
 import yaml
 from pathlib import Path
+import logging
 
 CONFIG_PATH = Path(__file__).parent / "config.yaml"
 
@@ -16,8 +17,22 @@ _config = load_config()
 
 OUTPUT_DIR = Path(_config["storage"]["output_dir"])
 TEMP_DIR = Path(_config["storage"]["temp_dir"])
+TEMP_DIR.mkdir(parents=True, exist_ok=True)
+
+LOG_FILE = Path(_config["storage"]["log_file"])
+LOG_FILE.parent.mkdir(exist_ok=True)
 
 DEPTH_MODEL_NAME = _config["models"]["depth"]["model_name"]
 SAM_CHUNK_SIZE = _config["models"]["layer_separation"]["chunk_size"]
 
-TEMP_DIR.mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,  # [DEBUG, INFO, WARNING, ERROR, CRITICAL]
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE, encoding="utf-8"),
+        logging.StreamHandler()  # вывод в консоль
+    ]
+)
+
+logger = logging.getLogger("AI_Video_Processor")
